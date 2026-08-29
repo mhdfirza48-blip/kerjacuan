e92eb67913543bbdc10a354fd6122b2d7e935cfa
mhdfirza48-blip <mhdfirza48@gmail.com>
2026-08-29 19:38:47 +0700

    Refactor Firebase imports and update config values

--- firebase.js
+++ firebase.js
@@ -1,21 +1,36 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+
 import {
-  getAuth, onAuthStateChanged, signInWithEmailAndPassword,
-  createUserWithEmailAndPassword, signOut
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
 import {
-  getFirestore, doc, getDoc, setDoc, addDoc, collection,
-  query, orderBy, getDocs, updateDoc, runTransaction, serverTimestamp
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  addDoc,
+  setDoc,
+  updateDoc,
+  query,
+  orderBy,
+  serverTimestamp,
+  runTransaction
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIZtNC_dqyP19htXNcdf3TZSDwGZqla5c",
-  authDomain: "kerjacuan-374dd.firebaseapp.com",
-  projectId: "kerjacuan-374dd",
-  storageBucket: "kerjacuan-374dd.firebasestorage.app",
+  apiKey: "AIzaSyCIZNCT_dqyP19htXNcdf3TZSDwGZqla5c",
+  authDomain: "kerjajuan-374dd.firebaseapp.com",
+  projectId: "kerjajuan-374dd",
+  storageBucket: "kerjajuan-374dd.firebasestorage.app",
   messagingSenderId: "677864393276",
   appId: "1:677864393276:web:b6bd6852dc8ede6904d139",
-  measurementId: "G-WZ27QDDH6L"
+  measurementId: "G-WZ7QDDH6L"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -23,8 +38,22 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export {
-  app, auth, db, onAuthStateChanged, signInWithEmailAndPassword,
-  createUserWithEmailAndPassword, signOut, doc, getDoc, setDoc,
-  addDoc, collection, query, orderBy, getDocs, updateDoc,
-  runTransaction, serverTimestamp
+  app,
+  auth,
+  db,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  addDoc,
+  setDoc,
+  updateDoc,
+  query,
+  orderBy,
+  serverTimestamp,
+  runTransaction
 };
